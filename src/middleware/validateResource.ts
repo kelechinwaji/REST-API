d05318977e9df25f060c09067513ff28184e8bbd
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2022-08-03 18:21:07 +0100

    created middleware to validate incoming paypayload

--- src/middleware/validateResource.ts
+++ src/middleware/validateResource.ts
@@ -1,8 +1,16 @@
 import { Request, Response, NextFunction} from "express"
 import {AnyZodObject} from "zod"
 
-const validate = () => () =>{
-
+const validate = (schema: AnyZodObject) => (req:Request, res:Response, next:NextFunction) =>{
+ try {
+    schema.parse({
+        body: req.body,
+        query: req.query,
+        params: req.params
+    });
+ } catch (e: any) {
+    return res.status(400).send(e.errors)
+ }
 }
 
 export default validate;
