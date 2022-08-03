0c668a713e5b61e94bea73a93b2ada4b98dbba9f
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2022-08-03 20:20:29 +0100

    completed handler for creating user

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,9 +1,11 @@
 import {Request, Response} from 'express';
+import { createUser } from '../service/user.service';
 import logger from '../utils/logger'
 
 export const createUserHandler= async (req: Request, res:Response)=>{
 try {
-    
+   const user = await createUser(req.body);
+   return user;
 } catch (e: any) {
    logger.error(e)
    return res.status(409).send(e.message)
--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,10 @@
 import { Express, Request, Response } from "express";
+import { createUserHandler } from "./controller/user.controller";
 
 const routes = (app: Express) =>{
- app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200))
+ app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200));
+
+ app.post('api/users', createUserHandler)
 }
 
 export default routes;
