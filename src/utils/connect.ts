8fb4539a56f42cda1594b234015025d930b79df4
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2022-08-03 11:21:35 +0100

    created database connection

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,8 +1,17 @@
 import mongoose from "mongoose";
 import config from "config"
 
-const connect = () =>{
+const  connect = async () =>{
+ const dbUri = config.get<string>("dbUri");
 
+ try {
+    await mongoose.connect(dbUri);
+    console.log('Database connected successfully');
+    
+ } catch (error) {
+   console.log('could not connect to Database');
+   process.exit(1)
+ }
 }
 
 export default connect
