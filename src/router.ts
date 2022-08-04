20276d507459410e74bfcabc5acb8397f3768c9d
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2022-08-04 21:05:24 +0100

    created an api endpoint callback function that uses the validate resource function

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,11 @@
 import { Express, Request, Response } from "express";
 import { createUserHandler } from "./controller/user.controller";
-
+import validateResource from "./middleware/validateResource";
+import {createUserShema} from "./schema/user.schema"
 const routes = (app: Express) =>{
  app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200));
 
- app.post('api/users', createUserHandler)
+ app.post('api/users', validateResource(createUserShema), createUserHandler)
 }
 
 export default routes;
