abdf8ed1aba6c8fe41f18ad025a8fac692a011c6
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2022-08-04 20:59:47 +0100

    used zod to implement password requirement

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -1,3 +1,21 @@
 import {object, string} from 'zod';
 
-export const createUserShema = object({})
+export const createUserShema = object({
+    body: object({
+        name: string({
+            required_error: "Name is required"
+        }),
+        password: string({
+            required_error:"Name is required"
+        }).min(6, "Password too short - should be 6 chars minimum").regex(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/),
+        passwordConfirmation: string({
+           required_error: "password confirmation is required" 
+        }),
+        email: string({
+           required_error: "Email is required" 
+        }).email("Not a valid email")
+    }).refine((data) => data.password === data.passwordConfirmation, {
+        message: "Password do not match",
+        path: ["passwordConfirmation"],
+    }),
+});
