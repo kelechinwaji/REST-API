e23ba23df1a1e428dc8e25b0da22d589162f13b1
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2022-08-06 08:24:34 +0100

    logic blueprint for session handler

---
+++ src/controller/session.controller.ts
@@ -0,0 +1,14 @@
+import {Response, Request} from "express"
+
+export const createUserSessioonHandler = (req:Request , res:Response)=>{
+
+    //Validate the user's password
+
+    //create a session
+
+    //create an access token
+
+    //create a refresh token
+
+    //return access and refresh tokens
+}
