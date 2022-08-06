7bd6fc6715ca68fcc6db06d95b890e7ef87cc93b
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2022-08-06 08:37:57 +0100

    implemeted function to validate user password

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,3 +1,4 @@
+import { compare } from 'bcrypt';
 import {DocumentDefinition} from 'mongoose';
 import UserModel, {userDocument} from '../models/user.model';
 
@@ -8,4 +9,14 @@ export const createUser = async (input: DocumentDefinition<Omit<userDocument, "c
  } catch (e: any) {
     throw new Error(e);
  }
+}
+
+export const validatePassword = async ({email, password}:{email: string, password: string}) =>{
+const user = await UserModel.findOne({email})
+
+if (!user){
+   return false
+}
+
+const isValid = await user.comparePassword(password)
 }
